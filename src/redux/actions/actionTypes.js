c66059a0d604f8fd07b68397dd109c2170030948
Nihat <senius.33@gmail.com>
2021-02-08 15:58:56 +0300

    some requests deleted

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -7,26 +7,20 @@ export const CURRENT_USER = "CURRENT_USER";
 
 // Movies
 
-export const GET_LATEST_MOVIE = "GET_LATEST_MOVIE";
 export const GET_TRENDING_MOVIES = "GET_TRENDING_MOVIES";
 export const GET_POPULAR_MOVIES = "GET_POPULAR_MOVIES";
 export const GET_ACTİON_MOVIES = "GET_ACTİON_MOVIES";
-export const GET_COMEDY_MOVIES = "GET_COMEDY_MOVIES";
 export const GET_ROMANCE_MOVIES = "GET_ROMANCE_MOVIES";
 export const GET_CRIME_MOVIES = "GET_CRIME_MOVIES";
 export const GET_DRAMA_MOVIES = "GET_DRAMA_MOVIES";
-export const GET_FAMILY_MOVIES = "GET_FAMILY_MOVIES";
 export const GET_WAR_MOVIES = "GET_WAR_MOVIES";
 
 // TV Shows
 
 export const GET_TRENDING_TV = "GET_TRENDING_TV";
-export const GET_LATEST_TV = "GET_LATEST_TV";
 export const GET_NETFLIX_ORIGINAL = "GET_NETFLIX_ORIGINAL";
 export const GET_POPULAR_TV = "GET_POPULAR_TV";
 export const GET_ACTİON_TV = "GET_ACTİON_TV";
-export const GET_COMEDY_TV = "GET_COMEDY_TV";
-export const GET_FAMILY_TV = "GET_FAMILY_TV";
 export const GET_CRIME_TV = "GET_CRIME_TV";
 export const GET_DRAMA_TV = "GET_DRAMA_TV";
 export const GET_WAR_TV = "GET_WAR_TV";
--- src/requests/requests.js
+++ src/requests/requests.js
@@ -2,25 +2,21 @@ const apiKey = "APIKEY";
 
 export const request = {
   // Movies
-  getLatestMovies: `/movie/latest?api_key=${apiKey}&language=en-US`,
+
   getTrendingMovies: `/trending/movies/week?api_key=${apiKey}&language=en-US`,
   getPopularMovies: `/movie/popular?api_key=${apiKey}&language=en-US`,
   getActionMovies: `/discover/movie?api_key=${apiKey}&with_genres=28`,
-  getComedyMovies: `/discover/movie?api_key=${apiKey}&with_genres=35`,
   getRomanceMovies: `/discover/movie?api_key=${apiKey}&with_genres=10749`,
   getCrimeMovies: `/discover/movie?api_key=${apiKey}&with_genres=80`,
   getDramaMovies: `/discover/movie?api_key=${apiKey}&with_genres=18`,
-  getFamilyMovies: `/discover/movie?api_key=${apiKey}&with_genres=10751`,
   getWarMovies: `/discover/movie?api_key=${apiKey}&with_genres=10752`,
 
   // TV Shows
-  getLatestTv: `/tv/latest?api_key=${apiKey}&language=en-US`,
+
   getTrendingTv: `/trending/tv/week?api_key=${apiKey}&language=en-US`,
   getNetflixOriginal: `/discover/tv?api_key=${apiKey}&with_networks=213`,
   getPopularTv: `/tv/popular?api_key=${apiKey}&language=en-US`,
-  getFamilyTvShows: `/discover/tv?api_key=${apiKey}&with_genres=10751`,
   getActionAdventureTvShows: `/discover/tv?api_key=${apiKey}&with_genres=10759`,
-  getComedyTvShows: `/discover/tv?api_key=${apiKey}&with_genres=35`,
   getCrimeTvShows: `/discover/tv?api_key=${apiKey}&with_genres=80`,
   getDramaTvShows: `/discover/tv?api_key=${apiKey}&with_genres=18`,
   getWarTvShows: `/discover/tv?api_key=${apiKey}&with_genres=10768`,
